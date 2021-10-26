f734fdeee36ae0deed796c507867a73e9f8cdf91
eraosso <osmar.sousa@gmail.com>
2021-10-26 09:12:41 -0300

    Terceiro commit.
    Falta arrumar a checagem dos  parametros.
    Impressão da equação está ok!

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,11 +1,21 @@
 def arithmetic_arranger(problems):
     arranged_problems = ""
     resultado = ""
+    resolve = None
 
-    if len(problems[0]) > 5:
+    if isinstance(problems, list):
+        if isinstance(problems[0], list) and len(problems) == 2:
+            resolve = problems[1]
+            problems = problems[0]
+        else:
+            if isinstance(problems[0], list):
+                problems = problems[0]
+                resolve = False
+
+    if len(problems) > 5:
         return "Error: Too many problems."
 
-    for problema in problems[0]:
+    for problema in problems:
         conta = problema.split()
         if conta[1] not in {"+", "-"}:
             return "Error: Operator must be '+' or '-'."
@@ -13,16 +23,22 @@ def arithmetic_arranger(problems):
             return "Error: Numbers must only contain digits."
         if (len(conta[0]) > 4) | (len(conta[2]) > 4):
             return "Error: Numbers cannot be more than four digits."
-    
-    qty = len(problems[0])
+
+    qty = len(problems)
 
     op = {'+': lambda x, y: int(x) + int(y), '-': lambda x, y: int(x) - int(y)}
+
+    def tamanho(l1, l2):
+        if l1 >= l2:
+            return l1
+        else:
+            return l2
 
     primeiro = []
     operacao = []
     segundo = []
 
-    for problema in problems[0]:
+    for problema in problems:
         primeiro.append(problema.split()[0])
         operacao.append(problema.split()[1])
         segundo.append(problema.split()[2])
@@ -32,23 +48,26 @@ def arithmetic_arranger(problems):
     dashes = ""
     resultado = ""
     for i in range(qty):
-        primeira_linha += (" " * (5 - len(primeiro[i])) + primeiro[i] + 4 * " ")
-        segunda_linha += operacao[i] + (" " * (5 - len(segundo[i])) + segundo[i] + 4 * " ")
-        dashes += "-" * 5 + " " * 4
+        l1 = len(primeiro[i])
+        l2 = len(segundo[i])
+        n_chars = tamanho(l1, l2)
+        primeira_linha += (" " * (n_chars + 2 - l1) + primeiro[i] + 4 * " ")
+        segunda_linha += operacao[i] + (" " * (n_chars + 1 - l2) + segundo[i] + 4 * " ")
+
+        dashes += "-" * (n_chars + 2) + " " * 4
         conta = str(op[operacao[i]](primeiro[i], segundo[i]))
-        resultado += (" " * (5 - len(str(conta))) + conta + 4 * " ")
+        resultado += (" " * (n_chars + 2 - len(str(conta))) + conta + 4 * " ")
 
     primeira_linha = primeira_linha[:-4] + "\n"
     segunda_linha = segunda_linha[:-4] + "\n"
     dashes = dashes[:-4]
     resultado = resultado[:-4]
 
-    if len(problems) == 2:
-        problems, resolve = problems
-        if resolve:
-            arranged_problems = primeira_linha + segunda_linha + dashes + "\n" + resultado
+    if resolve:
+        arranged_problems = primeira_linha + segunda_linha + dashes + "\n" + resultado
     else:
         arranged_problems = primeira_linha + segunda_linha + dashes
 
     return arranged_problems
+
 
